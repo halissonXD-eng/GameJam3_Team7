274cbfb5c814097a29e457f6eb266f760afccd38
Suly Hernandez <sunhdaystudio85@gmail.com>
2024-11-13 09:39:32 -0500

    UI Menú Inicial
    Botón Start
    Botón Settings
    Botón Credits

---
+++ Assets/Scripts/StartMenu.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    // Update is called once per frame
+    public void OpenSettings()
+    {
+        Debug.Log("Loading...");
+    }
+
+    public void OpenCredits()
+    {
+        Debug.Log("Loading...");
+    }
+}
